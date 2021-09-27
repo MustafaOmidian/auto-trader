aea75cb714da65fadbdfe60848b6a16376167638
Mustafa Omidian <60924400+MustafaOmidian@users.noreply.github.com>
2021-09-28 01:31:31 +0330

    Update auto.js

--- auto.js
+++ auto.js
@@ -5,3 +5,15 @@ var url = 'https://api.nobitex.ir/auth/login/'
 var headers = {
     "Content-Type": "application/json"
 }
+username = "name@example.com";
+password = "secret-password-1234";
+var form = {"username":username,"password":password};
+
+
+request({headers: headers,url: url, form: form, method:'Post'}, (err, res, body) => {
+    if (err) {
+        //console.log(err);
+    } else {
+        console.log('body:', body);
+    }
+  });
